79ecea4a39e326a3c08da84074410f604eba67ae
Krittin <krittin.ch2543@gmail.com>
2023-03-29 11:54:33 +0700

    config.js: playing to streaming, remove DJ, defaultVol to vol

--- config.js
+++ config.js
@@ -2,22 +2,16 @@ module.exports = {
   app: {
     token:
       "MTA4OTcyNDQ0NzYxMjc0Mzc5MA.GnPmKN.jqv_dTIx0L2nWuy1Jy8SpGYZCCqMFhWvxBnqHo",
-    playing: "icutmyhair ❤️",
     global: true,
     guild: "698559672864604230",
   },
 
   opt: {
-    DJ: {
-      enabled: false,
-      roleName: "",
-      commands: [],
-    },
     maxVol: 100,
     leaveOnEnd: true,
     loopMessage: false,
     spotifyBridge: true,
-    defaultvolume: 75,
+    volume: 75,
     discordPlayer: {
       ytdlOptions: {
         quality: "highestaudio",
